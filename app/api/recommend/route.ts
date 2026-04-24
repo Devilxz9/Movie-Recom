42b4b46052314c3209b38296aa60e53a68e7b323
Devilxz9 <pk675061@gmail.com>
2026-04-24 23:19:44 +0530

    added saved button and not show movies which are already saved

--- app/api/recommend/route.ts
+++ app/api/recommend/route.ts
@@ -40,7 +40,7 @@ export async function POST(req: Request) {
     }
   })
 
-  const savedTitles = savedMovies.map(e=> e.title)
+  const savedTitles = savedMovies.map((e:any)=> e.title)
   try {
     const { preferences } = await req.json();
 
